fcc4e221ab23b7b6c95dc138decc0e46c8961430
zhong <zhongchen@google.com>
2019-12-23 15:41:56 -0800

    build and move the kernel

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,4 +6,11 @@ if [[ "${ROLE}" == 'Master' ]]; then
   ssh-keyscan -t rsa github.com >  /root/.ssh/known_hosts
 
   git clone git://github.com/zhongchen/dataproc-kedro-demo.git /home/dataproc-kedro-demo
+
+  cd /home/dataproc-kedro-demo
+
+  pipenv install --python 3.6 -r requirements.txt
+  pipenv shell
+  python -m ipykernel install --user --name=kedro
+  sudo mv /root/.local/share/jupyter/kernels/kedro/ /opt/conda/anaconda/share/jupyter/kernels/
 fi
